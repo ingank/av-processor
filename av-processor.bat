a1dd8628e0994a2b26e1b87b8843486b1cfff569
Ingolf Ankert <github@ingank.net>
2023-01-18 18:10:25 +0100

    Update av-processor.bat

--- av-processor.bat
+++ av-processor.bat
@@ -1,18 +1,20 @@
 @echo off
 
-:START
+rem av-processor.bat
+rem 
+rem Diese Batchdatei unterstuetzt dich beim Herunterladen und
+rem Konvertieren von Videos und Audio-Dateien. Im gleichen Verzeichnis
+rem muessen sich die Programme "yt-dlp.exe" und "ffmpeg.exe" befinden.
+rem 
+rem Zu konvertierende Dateien muessen vorher in den Ordner "IN" kopiert werden.
+rem Heruntergeladene Dateien landen im Ordner "IN".
+rem Konvertierte Dateien landen im Ordner "OUT".
+
+:MENU
 cls
 echo ============
 echo AV-PROCESSOR
 echo ============
-echo.
-echo Diese Batchdatei unterstuetzt dich beim Herunterladen und
-echo Konvertieren von Videos und Audio-Dateien. Im gleichen Verzeichnis
-echo muessen sich die Programme ^"yt-dlp.exe^" und ^"ffmpeg.exe^" befinden.
-echo.
-echo Zu konvertierende Dateien muessen vorher in den Ordner ^"IN^" kopiert werden.
-echo Heruntergeladene Dateien landen automatisch im Ordner ^"IN^".
-echo Konvertierte Dateien landen im Ordner ^"OUT^".
 echo.
 echo Bitte triff deine Wahl:
 echo.
@@ -25,23 +27,28 @@ echo e - Programm beenden
 echo.
 set /p choice="Deine Wahl: "
 echo.
-if "%choice%" EQU "1" GOTO ONESHOTVIDEO
-if "%choice%" EQU "2" GOTO PLAYLISTVIDEOS
-if "%choice%" EQU "3" GOTO ONESHOTFLAC
-if "%choice%" EQU "4" GOTO PLAYLISTFLAC
-if "%choice%" EQU "5" GOTO CONVERTAUDIO
-if "%choice%" EQU "e" GOTO END
-GOTO START
+if "%choice%" EQU "1" goto ONESHOTVIDEO
+if "%choice%" EQU "2" goto PLAYLISTVIDEOS
+if "%choice%" EQU "3" goto ONESHOTFLAC
+if "%choice%" EQU "4" goto PLAYLISTFLAC
+if "%choice%" EQU "5" goto CONVERTAUDIO
+if "%choice%" EQU "e" goto END
+goto MENU
 
 :ONESHOTVIDEO
-mkdir "IN" 2>nul
-echo Die Videos werden als Einzelvideo heruntergeladen.
-echo Playlisten werden ignoriert.
+cls
+echo =========================
+echo EINZELVIDEO HERUNTERLADEN
+echo =========================
 echo.
-:LOOP_OSV
+echo Der Link wird als Einzelvideo behandelt.
+echo Playlisten werden ignoriert. Nur ein Video wird heruntergeladen.
+echo.
+echo Durch Eingabe von ^"e^" kommst du zurueck ins Hauptmenue.
 echo.
 set /p url="YouTube-Link: "
-echo.
+if "%url%" EQU "e" goto MENU
+mkdir "IN" 2>nul
 .\yt-dlp ^
 --no-playlist ^
 --no-keep-video ^
@@ -51,19 +58,22 @@ echo.
 --write-thumbnail ^
 --convert-thumbnails png ^
 --paths .\IN "%url%"
-echo.
-echo.
-GOTO LOOP_OSV
+goto ONESHOTVIDEO
 
 :PLAYLISTVIDEOS
-mkdir "IN" 2>nul
-echo Der Link wird als Playlist behandelt.
-echo Alle Videos der Playlist werden heruntergeladen.
+cls
+echo ============================
+echo PLAYLISTVIDEOS HERUNTERLADEN
+echo ============================
 echo.
-:LOOP_PLV
+echo Der Link wird als Playlist behandelt.
+echo Alle Videos einer Playlist werden heruntergeladen.
+echo.
+echo Durch Eingabe von ^"e^" kommst du zurueck ins Hauptmenue.
 echo.
 set /p url="YouTube-Link: "
-echo.
+if "%url%" EQU "e" goto MENU
+mkdir "IN" 2>nul
 .\yt-dlp ^
 --yes-playlist ^
 --no-keep-video ^
@@ -73,19 +83,23 @@ echo.
 --write-thumbnail ^
 --convert-thumbnails png ^
 --paths .\IN "%url%"
-echo.
-echo.
-GOTO LOOP_PLV
+goto PLAYLISTVIDEOS
 
 :ONESHOTFLAC
-mkdir "IN" 2>nul
-echo Die Audiospur eines Einzelvideos wird heruntergeladen
-echo und in einer .flac Datei gespeichert. Playlisten werden ignoriert.
+cls
+echo =====================================
+echo FLAC EINES EINZELVIDEOS HERUNTERLADEN
+echo =====================================
 echo.
-:LOOP_OSF
+echo Der Link wird als Einzelvideo behandelt.
+echo Playlisten werden ignoriert.
+echo Die Audiospur des Videos wird als .flac Datei gespeichert.
+echo.
+echo Durch Eingabe von ^"e^" kommst du zurueck ins Hauptmenue.
 echo.
 set /p url="YouTube-Link: "
-echo.
+if "%url%" EQU "e" goto MENU
+mkdir "IN" 2>nul
 .\yt-dlp ^
 --no-playlist ^
 --no-keep-video ^
@@ -96,19 +110,22 @@ echo.
 --convert-thumbnails png ^
 --paths .\IN ^
 --extract-audio --audio-format flac "%url%"
-echo.
-echo.
-GOTO LOOP_OSF
+goto ONESHOTFLAC
 
 :PLAYLISTFLAC
-mkdir "IN" 2>nul
+cls
+echo =================================================
+echo ALLE VIDEOS EINER PLAYLIST ALS FLAC HERUNTERLADEN
+echo =================================================
+echo.
 echo Der Link wird als Playlist behandelt. Von allen Videos der Playlist
 echo werden die Audiospuren in .flac Dateien gespeichert.
 echo.
-:LOOP_PLF
+echo Durch Eingabe von ^"e^" kommst du zurueck ins Hauptmenue.
 echo.
 set /p url="YouTube-Link: "
-echo.
+if "%url%" EQU "e" goto MENU
+mkdir "IN" 2>nul
 .\yt-dlp ^
 --yes-playlist ^
 --no-keep-video ^
@@ -119,43 +136,46 @@ echo.
 --convert-thumbnails png ^
 --paths .\IN ^
 --extract-audio --audio-format flac "%url%"
-echo.
-echo.
-GOTO LOOP_PLF
+goto PLAYLISTFLAC
 
 :CONVERTAUDIO
-echo Bitte gib das Quellformat an:
+cls
+echo ===================
+echo AUDIOFORMAT WANDELN
+echo ===================
+echo.
+echo Alle gleichartiegen Dateien im Ordner ^"IN^" werden konvertiert.
+echo Die kovertierten Dateien landen im Ordner ^"OUT^".
+echo.
+echo Bitte triff deine Wahl fuer das Quellformat!
 echo.
 echo 1 - flac
 echo 2 - mp3
 echo 3 - wav
 echo 4 - aiff
 echo 5 - aifc
-echo e - Programm beenden
+echo e - zurueck zum Hauptmenue
 echo.
 set /p choice="Deine Wahl: "
-echo.
 if "%choice%" EQU "1" set inf=flac
 if "%choice%" EQU "2" set inf=mp3
 if "%choice%" EQU "3" set inf=wav
 if "%choice%" EQU "4" set inf=aiff
 if "%choice%" EQU "5" set inf=aifc
-if "%choice%" EQU "e" GOTO END
-
-echo Bitte gib das gewuenschte Ausgabeformat an:
+if "%choice%" EQU "e" goto MENU
+echo.
+echo Bitte triff jetzt deine Wahl fuer das Ausgabeformat!
 echo.
 echo 1 - flac
 echo 2 - mp3
 echo 3 - wav
-echo e - Programm beenden
+echo e - zurueck zum Hauptmenue
 echo.
 set /p choice="Deine Wahl: "
-echo.
 if "%choice%" EQU "1" set outf=flac
 if "%choice%" EQU "2" set outf=mp3
 if "%choice%" EQU "3" set outf=wav
-if "%choice%" EQU "e" GOTO END
-
+if "%choice%" EQU "e" goto MENU
 mkdir "OUT" 2>nul
 cd IN
 if "%outf%" == "mp3" (
@@ -164,5 +184,6 @@ if "%outf%" == "mp3" (
     for %%f in ("*.%inf%") do (  .\..\ffmpeg -i "%%f" ".\..\OUT\%%~nf.%outf%" )
 )
 cd ..
+goto CONVERTAUDIO
 
 :END
